fb082cde1bc58d09aa4506636225fc4c4ce2ac89
Steven Price <steve@techrockstars.com>
2012-04-30 23:38:23 -0700

    Inital Add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SglTimer::Application.config.secret_token = '1ad59da2ed7210ed2e17f54a7e8c9f3698bc12c91342e5fd5e0120af76d7961ffc3a8cb7cc82af19f04cd698c41c7896b7e56a83b307efd98348c159253eeced'
